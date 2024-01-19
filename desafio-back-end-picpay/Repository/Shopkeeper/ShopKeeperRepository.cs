1da986b8c5143ccce0897ce0bb649983d9ce0d6c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-19 09:52:18 -0300

    Created ShopKeeperRepository class

---
+++ desafio-back-end-picpay/Repository/Shopkeeper/ShopKeeperRepository.cs
@@ -0,0 +1,8 @@
+﻿using desafio_back_end_picpay.Models;
+using desafio_back_end_picpay.Repository.Generic;
+
+namespace desafio_back_end_picpay.Repository.Shopkeeper;
+
+public class ShopkeeperRepository: GenericRepository<ShopKeeper>, IShopKeeperRepository
+{
+}
