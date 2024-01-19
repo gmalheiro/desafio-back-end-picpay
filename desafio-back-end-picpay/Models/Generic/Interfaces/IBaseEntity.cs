f70b0cfadac8c5e20d2085913300003f59bdbbf9
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-19 09:49:46 -0300

    Created BaseEntity interface

---
+++ desafio-back-end-picpay/Models/Generic/Interfaces/IBaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace desafio_back_end_picpay.Models.Generic.Interfaces;
+
+public interface IBaseEntity
+{
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
