660357bf70aad2104243d964798861789faeebe5
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-19 10:55:52 -0300

    Renamed client to base entity

--- desafio-back-end-picpay/Models/Generic/Interfaces/IClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace desafio_back_end_picpay.Models.Generic.Interfaces;
-
-public interface IClient
-{
-    public string? FullName { get; set; }
-    public string? Email { get; set; }
-    public string? Password { get; set; }
-}
