8b35a2423575db24bc0a9ef144644440fc893365
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-19 10:57:23 -0300

    Created infrastructure module

---
+++ desafio-back-end-picpay/Infrastructure/InfrastructureModule.cs
@@ -0,0 +1,21 @@
+﻿using desafio_back_end_picpay.Repository.Shopkeeper;
+
+namespace desafio_back_end_picpay.Infrastructure;
+
+public static class InfrastructureModule
+{
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+        services
+            .AddRepositories();
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IShopKeeperRepository,ShopkeeperRepository>();
+
+        return services;
+    }
+}
