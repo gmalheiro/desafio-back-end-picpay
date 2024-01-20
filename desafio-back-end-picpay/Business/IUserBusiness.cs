012f25b21982cb16a6ec534e7071f91547715a94
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2024-01-19 21:23:16 -0300

    Created IUserBusiness interface

---
+++ desafio-back-end-picpay/Business/IUserBusiness.cs
@@ -0,0 +1,13 @@
+﻿using desafio_back_end_picpay.Data.DTOs;
+using desafio_back_end_picpay.Models;
+
+namespace desafio_back_end_picpay.Business;
+
+public interface IUserBusiness
+{
+    List<UserDTO> FindAll();
+    UserDTO GetById(int id);   
+    UserDTO Create(UserDTO user);
+    UserDTO Update(UserDTO user); 
+    UserDTO Delete(int id);
+}
