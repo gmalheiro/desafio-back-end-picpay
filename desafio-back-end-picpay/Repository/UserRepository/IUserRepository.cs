722dafd6332f27030a277318ff601db1f1ffdf4a
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-20 00:21:37 -0300

    Added method FindUserByName in repository

--- desafio-back-end-picpay/Repository/UserRepository/IUserRepository.cs
+++ desafio-back-end-picpay/Repository/UserRepository/IUserRepository.cs
@@ -1,8 +1,10 @@
-﻿using desafio_back_end_picpay.Models;
+﻿using desafio_back_end_picpay.Data.DTOs;
+using desafio_back_end_picpay.Models;
 using desafio_back_end_picpay.Repository.Generic;
 
 namespace desafio_back_end_picpay.Repository.UserRepository;
 
 public interface IUserRepository : IRepository<User>
 {
+    User FindUserByName(string name);
 }
--- desafio-back-end-picpay/Repository/UserRepository/UserRepository.cs
+++ desafio-back-end-picpay/Repository/UserRepository/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using desafio_back_end_picpay.Data.Context;
+using desafio_back_end_picpay.Data.DTOs;
 using desafio_back_end_picpay.Models;
 using desafio_back_end_picpay.Repository.Generic;
+using System.Data;
 
 namespace desafio_back_end_picpay.Repository.UserRepository;
 
@@ -8,5 +10,14 @@ public class UserRepository : GenericRepository<User>, IUserRepository
 {
     public UserRepository(DatabaseContext context) : base(context)
     {
+
+    }
+
+    public User FindUserByName(string name)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.FullName == name );
+        if (user is null)
+            return new User();
+        return user;
     }
 }
