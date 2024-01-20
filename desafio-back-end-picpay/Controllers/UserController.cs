b1dc9979aa71d7716d3da3735e5216042fc364ca
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-20 18:40:04 -0300

    Update in UserController  added all the methods

--- desafio-back-end-picpay/Controllers/UserController.cs
+++ desafio-back-end-picpay/Controllers/UserController.cs
@@ -1,7 +1,6 @@
 ﻿using desafio_back_end_picpay.Business;
-using Microsoft.AspNetCore.Http;
+using desafio_back_end_picpay.Data.DTOs;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace desafio_back_end_picpay.Controllers;
 
@@ -16,10 +15,71 @@ public class UserController : ControllerBase
         _business = business;
     }
 
-    [HttpGet]
+    [HttpGet("findAll")]
+    [ProducesResponseType(typeof(List<UserDTO>), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
     public IActionResult FindAll()
     {
         var users = _business.FindAll();
         return Ok(users);
+    }
+
+    [HttpGet("findById/{id}")]
+    [ProducesResponseType(typeof(UserDTO), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult FindById([FromRoute]int id)
+    {
+        var person = _business.GetById(id);
+        if (person is null) 
+            return NotFound();
+        return Ok(person);
+    }
+
+    [HttpGet("findByName")]
+    [ProducesResponseType(typeof(UserDTO), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult FindByName([FromQuery]string user)
+    {
+        var person = _business.FindUserByName(user);
+        if (person == null) return NotFound();
+        return Ok(person);
+    }
+
+    [HttpPost("createUser")]
+    [ProducesResponseType(typeof(UserDTO), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Post([FromBody] UserDTO user)
+    {
+        if (user == null) return BadRequest();
+        return Ok(_business.Create(user));
+    }
+
+    [HttpPut("updateUser")]
+    [ProducesResponseType(typeof(UserDTO), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Put([FromBody] UserDTO user)
+    {
+        if (user == null) return BadRequest();
+        return Ok(_business.Update(user));
+    }
+
+    [HttpDelete("deleteUser/{name}")]
+    [ProducesResponseType(typeof(UserDTO), 200)]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Delete(string name)
+    {
+        var user = _business.FindUserByName(name);
+        _business.Delete(user.Id);
+        return NoContent();
     }
 }
