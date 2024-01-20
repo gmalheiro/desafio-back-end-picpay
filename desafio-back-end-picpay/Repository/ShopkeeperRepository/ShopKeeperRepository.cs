36682487922a9c8e64626bc7911482a449ffddf9
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-19 23:34:41 -0300

    Update in Repositories

--- desafio-back-end-picpay/Repository/ShopkeeperRepository/ShopKeeperRepository.cs
+++ desafio-back-end-picpay/Repository/ShopkeeperRepository/ShopKeeperRepository.cs
@@ -1,8 +1,12 @@
-﻿using desafio_back_end_picpay.Models;
+﻿using desafio_back_end_picpay.Data.Context;
+using desafio_back_end_picpay.Models;
 using desafio_back_end_picpay.Repository.Generic;
 
 namespace desafio_back_end_picpay.Repository.ShopKeeperRepository;
 
 public class ShopkeeperRepository: GenericRepository<ShopKeeper>, IShopKeeperRepository
 {
+    public ShopkeeperRepository(DatabaseContext context) : base(context)
+    {
+    }
 }
--- desafio-back-end-picpay/Repository/UserRepository/UserRepository.cs
+++ desafio-back-end-picpay/Repository/UserRepository/UserRepository.cs
@@ -1,8 +1,12 @@
-﻿using desafio_back_end_picpay.Models;
+﻿using desafio_back_end_picpay.Data.Context;
+using desafio_back_end_picpay.Models;
 using desafio_back_end_picpay.Repository.Generic;
 
 namespace desafio_back_end_picpay.Repository.UserRepository;
 
 public class UserRepository : GenericRepository<User>, IUserRepository
 {
+    public UserRepository(DatabaseContext context) : base(context)
+    {
+    }
 }
