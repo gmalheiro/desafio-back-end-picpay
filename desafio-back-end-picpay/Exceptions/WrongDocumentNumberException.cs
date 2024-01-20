e96463c53bcb41b86158f256c221e4064f3d7c14
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-20 18:50:18 -0300

    Created wrong documnet number exception class

---
+++ desafio-back-end-picpay/Exceptions/WrongDocumentNumberException.cs
@@ -0,0 +1,6 @@
+﻿namespace desafio_back_end_picpay.Exceptions;
+
+public class WrongDocumentNumberException : ApplicationException
+{
+    public WrongDocumentNumberException(string message) : base(message){}
+}
