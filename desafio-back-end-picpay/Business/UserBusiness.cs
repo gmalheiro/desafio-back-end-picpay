346debcf3f8fd1ede197355b808ec5d0a5fcd568
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-20 21:34:07 -0300

    Added new validations and possible exceptions

--- desafio-back-end-picpay/Business/UserBusiness.cs
+++ desafio-back-end-picpay/Business/UserBusiness.cs
@@ -1,24 +1,19 @@
-﻿using desafio_back_end_picpay.Data.Context;
-using desafio_back_end_picpay.Data.DTOs;
+﻿using desafio_back_end_picpay.Data.DTOs;
 using desafio_back_end_picpay.Exceptions;
 using desafio_back_end_picpay.Models;
 using desafio_back_end_picpay.Repository.UserRepository;
 using desafio_back_end_picpay.Utils;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
-using System.Data;
 
 namespace desafio_back_end_picpay.Business;
 
 public class UserBusiness : IUserBusiness
 {
     private readonly IUserRepository _repository;
-    private readonly DatabaseContext _dbContext;
 
-    public UserBusiness(IUserRepository repository, DatabaseContext dbContext)
+    public UserBusiness(IUserRepository repository)
     {
         _repository = repository;
-        _dbContext = dbContext;
     }
 
     public UserDTO Create(UserDTO user)
@@ -31,8 +26,20 @@ public class UserBusiness : IUserBusiness
                 Email = user.Email,
                 Password = user.Password,
                 Balance = user.Balance,
-                Cpf = UtilsClass.RemoveSpecialCharacters(user.CPF),
+                DocumentNumber = UtilsClass.RemoveSpecialCharacters(user.DocumentNumber),
+                IsPessoaFisica = user.IsPessoaFisica
             };
+
+            var userInDb = _repository.FindUserByName(userToBeCreated.FullName);
+
+            if (userInDb != null || userInDb?.Email == user.Email || userInDb?.DocumentNumber == user.DocumentNumber)
+                throw new NotPossibleOperation("It's not possible to create user, user already exists in database! Try using a different email and document number");
+
+            if (user.IsPessoaFisica && UtilsClass.DetectSpecialCharacters(user.DocumentNumber))
+                userToBeCreated!.DocumentNumber = UtilsClass.RemoveSpecialCharacters(user.DocumentNumber);
+            else
+                userToBeCreated!.DocumentNumber = user.DocumentNumber;
+
 
             _repository.Create(userToBeCreated);
 
@@ -51,13 +58,22 @@ public class UserBusiness : IUserBusiness
 
         _repository.Delete(id);
 
+
+        string userDocument;
+
+        if (userInDb.IsPessoaFisica)
+            userDocument = UtilsClass.FormatCPF(userInDb?.DocumentNumber ?? "");
+        else
+            userDocument = UtilsClass.FormatCNPJ(userInDb?.DocumentNumber ?? "");
+
         var user = new UserDTO
             (
                 userInDb?.FullName ?? "",
                 userInDb?.Email ?? "",
                 userInDb?.Password ?? "",
                 userInDb?.Balance ?? 0,
-                UtilsClass.FormatCPF(userInDb?.Cpf ?? "")
+                userDocument,
+                userInDb?.IsPessoaFisica ?? true
             );
 
         return user;
@@ -67,17 +83,26 @@ public class UserBusiness : IUserBusiness
     {
         var users = new List<UserDTO>();
 
-        var usersInDB = _repository.FindAll(); 
+        var usersInDB = _repository.FindAll();
 
         foreach (var userInDB in usersInDB)
         {
+
+            string userDocument;
+
+            if (userInDB.IsPessoaFisica)
+                userDocument = UtilsClass.FormatCPF(userInDB?.DocumentNumber ?? "");
+            else
+                userDocument = UtilsClass.FormatCNPJ(userInDB?.DocumentNumber ?? "");
+
             var user = new UserDTO
             (
                 userInDB?.FullName ?? "",
                 userInDB?.Email ?? "",
                 userInDB?.Password ?? "",
                 userInDB?.Balance ?? 0,
-                UtilsClass.FormatCPF(userInDB?.Cpf ?? "")
+                userDocument,
+                userInDB?.IsPessoaFisica ?? false
             );
 
             users.Add(user);
@@ -89,6 +114,9 @@ public class UserBusiness : IUserBusiness
     public User FindUserByName(string name)
     {
         var userInDb = _repository.FindUserByName(name);
+
+        if (userInDb is null)
+            throw new NotFoundException("User was not found");
 
         return userInDb;
 
@@ -102,13 +130,22 @@ public class UserBusiness : IUserBusiness
 
             if (userInDb != null)
             {
+
+                string userDocument;
+
+                if (userInDb.IsPessoaFisica)
+                    userDocument = UtilsClass.FormatCPF(userInDb?.DocumentNumber ?? "");
+                else
+                    userDocument = UtilsClass.FormatCNPJ(userInDb?.DocumentNumber ?? "");
+
                 var user = new UserDTO
                 (
                     userInDb?.FullName ?? "",
                     userInDb?.Email ?? "",
                     userInDb?.Password ?? "",
                     userInDb?.Balance ?? 0,
-                    UtilsClass.FormatCPF(userInDb?.Cpf ?? "")
+                    userDocument,
+                    userInDb?.IsPessoaFisica ?? true
                 );
 
                 return user;
@@ -132,28 +169,29 @@ public class UserBusiness : IUserBusiness
             var userEntity = _repository.FindUserByName(user.FullName);
 
             if (userEntity == null)
-            {
                 throw new NotFoundException($"User with name {user.FullName} not found");
-            }
 
-            userEntity.FullName = user.FullName;
+            string userDocument;
+
+            if (userEntity.IsPessoaFisica)
+                userDocument = UtilsClass.FormatCPF(userEntity?.DocumentNumber ?? "");
+            else
+                userDocument = UtilsClass.FormatCNPJ(userEntity?.DocumentNumber ?? "");
+
+            userEntity!.FullName = user.FullName;
             userEntity.Email = user.Email;
             userEntity.Password = user.Password;
             userEntity.Balance = user.Balance;
-            userEntity.Cpf = UtilsClass.RemoveSpecialCharacters(user.CPF);
+            userEntity.DocumentNumber = userDocument;
 
             _repository.Update(userEntity);
 
             return user;
         }
-        catch (NotFoundException)
-        {
-            throw;
-        }
         catch (Exception e)
         {
             Log.Error($"An error occurred while updating the user: {e}");
-            throw new NotFoundException("An error occurred while updating the user.");
+            throw new Exception("An error occurred while updating the user.");
         }
     }
 
