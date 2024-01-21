6a3410f5103b699835c128ca5315253f57db526b
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-21 19:20:51 -0300

    Created ITransactionBusiness

---
+++ desafio-back-end-picpay/Business/TransactionBusiness/ITransactionBusiness.cs
@@ -0,0 +1,8 @@
+﻿using desafio_back_end_picpay.Data.DTOs;
+
+namespace desafio_back_end_picpay.Business.TransactionBusiness;
+
+public interface ITransactionBusiness
+{
+    TransactionDTO Payment(TransactionDTO paymentInfo);
+}
