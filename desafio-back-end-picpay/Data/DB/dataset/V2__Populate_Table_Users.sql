357edd85f09f2c33b0101d66e3568d360d886c3f
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-20 21:37:07 -0300

    Populated table users with users and stores

--- desafio-back-end-picpay/Data/DB/dataset/V2__Populate_Table_Users.sql
+++ desafio-back-end-picpay/Data/DB/dataset/V2__Populate_Table_Users.sql
@@ -1,5 +1,6 @@
-INSERT INTO Users (full_name, email, password, balance, cpf)
+INSERT INTO Users (full_name, email, password, balance, document_number, is_pessoa_fisica)
 VALUES
-    ('John Doe', 'john.doe@example.com', 'password123', 1000.00, '12345678901'),
-    ('Jane Smith', 'jane.smith@example.com', 'securepass', 1500.50, '98765432109'),
-    ('Bob Johnson', 'bob.johnson@example.com', 'pass123', 800.75, '56789012345');
+    ('John Doe', 'john.doe@example.com', 'password123', 1000.00, '12345678909', 1), -- CPF (true)
+    ('Jane Smith', 'jane.smith@example.com', 'pass456', 1500.50, '12345678000190', 0), -- CNPJ (false)
+    ('Bob Johnson', 'bob.johnson@example.com', 'secret789', 2000.75, '98765432101', 1), -- CPF (true)
+    ('Acme Corporation', 'info@acme.com', 'securepwd', 5000.25, '87654321000123', 0); -- CNPJ (false)
