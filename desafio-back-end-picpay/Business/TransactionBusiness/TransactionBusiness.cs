c045f078fe46f73ae0f43b405ae5be2bbafd3294
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-21 19:21:11 -0300

    Created TransactionBusiness

---
+++ desafio-back-end-picpay/Business/TransactionBusiness/TransactionBusiness.cs
@@ -0,0 +1,11 @@
+﻿using desafio_back_end_picpay.Data.DTOs;
+
+namespace desafio_back_end_picpay.Business.TransactionBusiness;
+
+public class TransactionBusiness : ITransactionBusiness
+{
+    public TransactionDTO Payment(TransactionDTO paymentInfo)
+    {
+        throw new NotImplementedException();
+    }
+}
