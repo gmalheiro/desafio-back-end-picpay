1066a368edc801104ffdeaad5f885b5dfab33918
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-20 21:43:36 -0300

    Created  TransactionController and payment endpoint

---
+++ desafio-back-end-picpay/Controllers/TransactionController.cs
@@ -0,0 +1,20 @@
+﻿using desafio_back_end_picpay.Business;
+using desafio_back_end_picpay.Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace desafio_back_end_picpay.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionController : ControllerBase
+{
+    [HttpPost("payment")]
+    [ProducesResponseType(typeof(TransactionDTO), 200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(401)]
+    public IActionResult Post([FromBody] TransactionDTO paymentInfo)
+    {
+        return Ok(paymentInfo);
+    }
+
+}
