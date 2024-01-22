21a54174c10ba652997f4d2281f6487c8d384d95
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-21 22:12:11 -0300

    Created Notification and authorization services

---
+++ desafio-back-end-picpay/Services/AuthorizationService.cs
@@ -0,0 +1,39 @@
+﻿using desafio_back_end_picpay.Models;
+using Newtonsoft.Json;
+
+namespace desafio_back_end_picpay.Services;
+
+public static class AuthorizationService
+{
+    public static async Task<bool> CallAuthorizationService()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string apiUrl = "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    var result = JsonConvert.DeserializeObject<ServiceModel>(responseContent);
+
+                    return result != null && result.Message == "Autorizado";
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ desafio-back-end-picpay/Services/NotificationService.cs
@@ -0,0 +1,39 @@
+﻿using desafio_back_end_picpay.Models;
+using Newtonsoft.Json;
+
+namespace desafio_back_end_picpay.Services;
+
+public static class NotificationService
+{
+    public static  async Task<bool> CallNotificationService()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string apiUrl = "https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+
+                    var result = JsonConvert.DeserializeObject<ServiceModel>(responseContent);
+
+                    return result != null && result.Message == "true";
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
