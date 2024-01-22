5f86d01ffac5c23ad343c4ff11932b7abc12205c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-01-21 22:13:09 -0300

    Created TransactionStatus model

---
+++ desafio-back-end-picpay/Data/DTOs/TransactionStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace desafio_back_end_picpay.Data.DTOs;
+
+public record TransactionStatus
+    (
+        string sent,
+        string operation,
+        string sender,
+        string receiver,
+        string message
+    );
